b4bd74c2518e327ab34f5ef756d3020f53909a0e
kiminkyu <koreaprogrammer@naver.com>
2025-12-26 14:01:40 +0900

    API 수정

--- services/groqService.ts
+++ services/groqService.ts
@@ -1,6 +1,6 @@
 import { SummaryLength, AnalysisResult, GeneratedContent, VideoMetadata } from "../types";
 
-const API_BASE_URL = "http://127.0.0.1:5000";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:5000";
 
 interface AnalyzeResponse {
   metadata: VideoMetadata;
